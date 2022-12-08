b9e0f2e65543d994abfffea2ba5cadd5b74214da
Lauro Martinez <lauroboy00@gmail.com>
2022-12-08 02:07:15 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int32_value, bool_value, double_value) VALUES (438, 0, 43792.490);
+
+--UPDATE sandbox SET int64_value = 23214 WHERE id = 8;
+
+--DELETE FROM sandbox WHERE id = 36;
